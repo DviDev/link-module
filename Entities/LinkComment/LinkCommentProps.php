d62aa1434d5bd72e7e1438af224e9bc240b1f675
Davi Menezes <davimenezes.dev@gmail.com>
2022-11-05 08:20:57 -0300

    refactorings upgrade base structure

---
+++ Entities/LinkComment/LinkCommentProps.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Link\Entities\LinkComment;
+
+/**
+ * @property $id
+ * @property $link_id
+ * @property $user_id
+ * @property $parent_id
+ * @property $message
+ * @property $created_at
+ */
+trait LinkCommentProps
+{
+
+}
