a7e962923750545d5816811a9f4eaff831024115
Davi Menezes <davimenezes.dev@gmail.com>
2022-11-05 08:20:57 -0300

    refactorings upgrade base structure

---
+++ Entities/Link/LinkProps.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Link\Entities\Link;
+
+/**
+ * @property $id
+ * @property $workspace_id
+ * @property $user_id
+ * @property $name
+ * @property $link_url
+ * @property $description
+ * @property $created_at
+ */
+trait LinkProps
+{
+
+}
---
+++ Entities/LinkTag/LinkTagProps.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\link\Entities\LinkTag;
+
+/**
+ * @property $id
+ * @property $link_id
+ * @property $tag
+ */
+trait LinkTagProps
+{
+
+}
