a5d9a64904b3584fb9e12973a72305b38215890a
Davi Menezes <davimenezes.dev@gmail.com>
2023-08-04 05:59:41 -0300

    refactorings

--- Resources/views/components/page/links_page.blade.php
+++ Resources/views/components/page/links_page.blade.php
@@ -1,3 +1,11 @@
-<x-app-layout>
-    <livewire:link-table/>
-</x-app-layout>
+<x-lte::layout.v1.page>
+    <x-slot:header>
+        <div class="flex justify-between">
+            <div class="my-auto text-gray-800 text-bold">Links</div>
+            <a href="{{route('builder.page', 1)}}" class="btn btn-primary">Structure</a>
+        </div>
+    </x-slot:header>
+    <x-slot:body>
+        <livewire:link::table/>
+    </x-slot:body>
+</x-lte::layout.v1.page>
