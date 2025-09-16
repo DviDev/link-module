f7d733bb2d7ce751129d3e4b724dee53b0adff06
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-16 00:10:15 -0300

    Remove redundant test methods from LinkTableTest and LinkTagTableTest to clean up test files

--- tests/Tables/LinkTagTableTest.php
+++ tests/Tables/LinkTagTableTest.php
@@ -17,44 +17,4 @@ class LinkTagTableTest extends BaseTest
     {
         return LinkTagModel::class;
     }
-
-    public function test_table_must_exist()
-    {
-        parent::tableMustExist();
-    }
-
-    public function test_table_has_expected_columns()
-    {
-        parent::tableHasExpectedColumns();
-    }
-
-    public function test_can_create_instance_of_entity()
-    {
-        parent::canCreateInstanceOfEntity();
-    }
-
-    public function test_can_create_instance_of_model()
-    {
-        parent::canCreateInstanceOfModel();
-    }
-
-    public function test_should_save($attributes = null)
-    {
-        parent::shouldSave($attributes);
-    }
-
-    public function test_should_update($attributes = null)
-    {
-        parent::shouldUpdate($attributes);
-    }
-
-    public function test_should_delete()
-    {
-        parent::shouldDelete();
-    }
-
-    protected function create(): LinkTagModel
-    {
-        return $this->getModelClass()::factory()->create();
-    }
 }
