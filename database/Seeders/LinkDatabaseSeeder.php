405a208d7c5acbdd3acf90506a608de2ea388b7e
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-25 11:06:43 -0300

    Move ProjectModuleModel to Schema module
    ProjectModuleModel has been relocated from the Project module to the Schema module

--- database/Seeders/LinkDatabaseSeeder.php
+++ database/Seeders/LinkDatabaseSeeder.php
@@ -10,7 +10,7 @@ use Modules\Base\Database\Seeders\BaseSeeder;
 use Modules\DBMap\Domains\ScanTableDomain;
 use Modules\Link\Models\LinkModel;
 use Modules\Permission\Database\Seeders\PermissionTableSeeder;
-use Modules\Project\Models\ProjectModuleModel;
+use Modules\Schema\Models\ProjectModuleModel;
 use Modules\Workspace\Models\WorkspaceModel;
 
 final class LinkDatabaseSeeder extends BaseSeeder
