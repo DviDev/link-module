da53241f0df77de4faa07bf4570206a16b3f4d12
Davi Menezes <davimenezes.dev@gmail.com>
2026-08-07 19:33:31 -0300

    lang path improvements to phpstorm autocomplete

--- app/Providers/LinkServiceProvider.php
+++ app/Providers/LinkServiceProvider.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Link\Providers;
 
-use Config;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Support\ServiceProvider;
 use Livewire;
+use Modules\Base\Contracts\BaseServiceProviderContract;
 use Modules\DBMap\Events\ScanTableEvent;
 use Modules\Link\Http\Livewire\Pages\LinksPage;
 use Modules\Link\Listeners\CreateMenuItemsListener;
@@ -18,42 +17,10 @@ use Modules\Project\Events\CreateMenuItemsEvent;
 use Modules\View\Events\DefineSearchableAttributesEvent;
 use Modules\View\Events\ElementPropertyCreatingEvent;
 
-final class LinkServiceProvider extends ServiceProvider
+final class LinkServiceProvider extends BaseServiceProviderContract
 {
-    /**
-     * @var string
-     */
-    protected $moduleName = 'Link';
-
-    /**
-     * @var string
-     */
-    protected $moduleNameLower = 'link';
-
-    /**
-     * Boot the application events.
-     *
-     * @return void
-     */
-    public function boot()
+    public function registerEvents(): void
     {
-        $this->registerTranslations();
-        $this->registerConfig();
-        $this->registerViews();
-
-        $this->registerComponents();
-
-        $this->loadMigrationsFrom(module_path($this->moduleName, 'database/Migrations'));
-    }
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->register(RouteServiceProvider::class);
 
         Event::listen(CreateMenuItemsEvent::class, CreateMenuItemsListener::class);
         Event::listen(DefineSearchableAttributesEvent::class, DefineSearchableAttributes::class);
@@ -61,65 +28,11 @@ final class LinkServiceProvider extends ServiceProvider
         Event::listen(ElementPropertyCreatingEvent::class, TranslateViewElementPropertiesLinkListener::class);
     }
 
-    /**
-     * Register views.
-     *
-     * @return void
-     */
-    public function registerViews()
+    public function provides(): array
     {
-        $viewPath = resource_path('views/modules/'.$this->moduleNameLower);
-
-        $sourcePath = module_path($this->moduleName, 'resources/views');
-
-        $this->publishes([
-            $sourcePath => $viewPath,
-        ], ['views', $this->moduleNameLower.'-module-views']);
-
-        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
-    }
-
-    /**
-     * Register translations.
-     *
-     * @return void
-     */
-    public function registerTranslations()
-    {
-        $langPath = resource_path('lang/modules/'.$this->moduleNameLower);
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom($langPath);
-        } else {
-            $this->loadTranslationsFrom(module_path($this->moduleName, 'lang'), $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom(module_path($this->moduleName, 'lang'));
-        }
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return [];
-    }
-
-    /**
-     * Register config.
-     *
-     * @return void
-     */
-    protected function registerConfig()
-    {
-        $this->publishes([
-            module_path($this->moduleName, 'config/config.php') => config_path($this->moduleNameLower.'.php'),
-        ], 'config');
-        $this->mergeConfigFrom(
-            module_path($this->moduleName, 'config/config.php'), $this->moduleNameLower
-        );
+        return [
+            RouteServiceProvider::class,
+        ];
     }
 
     protected function registerComponents(): void
@@ -127,15 +40,13 @@ final class LinkServiceProvider extends ServiceProvider
         Livewire::component('link::pages.links', LinksPage::class);
     }
 
-    private function getPublishableViewPaths(): array
+    public function getModuleName(): string
     {
-        $paths = [];
-        foreach (Config::get('view.paths') as $path) {
-            if (is_dir($path.'/modules/'.$this->moduleNameLower)) {
-                $paths[] = $path.'/modules/'.$this->moduleNameLower;
-            }
-        }
+        return 'Link';
+    }
 
-        return $paths;
+    public function getModuleNameLower(): string
+    {
+        return 'link';
     }
 }
--- lang/pt_BR/link_tags.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'id' => 'id',
-    'link_id' => 'link',
-    'tag' => 'tag',
-];
